62b1b85df114d418976e9fea7d2d7b531e3488a2
edwardsa <alex.edwards@flightcentre.com.au>
2013-10-18 13:35:32 +1000

    Added static helpers to MockitoVerifyRule

--- src/main/java/com/deepmock/mockito/MockitoVerifyRule.java
+++ src/main/java/com/deepmock/mockito/MockitoVerifyRule.java
@@ -13,15 +13,39 @@ import java.util.Collection;
 public class MockitoVerifyRule extends TestWatcher {
     private Object testInstance;
     private boolean onlyOnFailure;
+    private boolean verifyNoMore;
+
+    /**
+     * If the test fails, verify the expected calls have been executed
+     */
+    public static MockitoVerifyRule verifyOnFailure(Object testInstance) {
+        return new MockitoVerifyRule(testInstance, true, false);
+    }
+
+    /**
+     * Verify the expected calls have been executed
+     */
+    public static MockitoVerifyRule verifyAlways(Object testInstance) {
+        return new MockitoVerifyRule(testInstance, false, false);
+    }
+
+    /**
+     * Verify the expected calls have been executed and that no more invocations have occurred
+     */
+    public static MockitoVerifyRule verifyAlwaysStrict(Object testInstance) {
+        return new MockitoVerifyRule(testInstance, false, true);
+    }
 
     /**
      * Instantiate the rule with your test instance (e.g. this) and whether you want to verify only when there is an exception
      * @param testInstance The test instance
      * @param onlyOnFailure true if you only want to verify when the test fails, false to always verify
+     * @param verifyNoMore true if you want to verify there are no more unexpected mock invocations
      */
-    public MockitoVerifyRule(Object testInstance, boolean onlyOnFailure) {
+    public MockitoVerifyRule(Object testInstance, boolean onlyOnFailure, boolean verifyNoMore) {
         this.testInstance = testInstance;
         this.onlyOnFailure = onlyOnFailure;
+        this.verifyNoMore = verifyNoMore;
     }
 
     @Override
@@ -41,5 +65,8 @@ public class MockitoVerifyRule extends TestWatcher {
     private void verifyAllMocks() {
         Collection<Object> mockFields = AnnotationHelper.findAnnotatedFields(testInstance, Mock.class).values();
         Verify.verifyExpectations(mockFields.toArray());
+        if (verifyNoMore) {
+            Verify.verifyNoMoreInteractions(mockFields.toArray());
+        }
     }
 }
